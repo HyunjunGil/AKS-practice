8a70b942c3f31bbc8bc34cb32e8f35683a986f2a
rlfahs3025 <rlfahs3025@gmail.com>
2025-08-28 00:25:53 +0900

    Fix kafka

--- backend/generate_secret.py
+++ backend/generate_secret.py
@@ -1,13 +1,12 @@
 import secrets
 import base64
 
-secret_key = secrets.token_hex(32)
-encoded_key = base64.b64encode(secret_key.encode()).decode()
 
 mariadb_password_raw = ""
 mariadb_password_encoded = base64.b64encode(mariadb_password_raw.encode()).decode()
 
-print(mariadb_password_raw, mariadb_password_encoded)
+print("mariadb_password_raw: ", mariadb_password_raw)
+print("mariadb_password_encoded: ", mariadb_password_encoded)
 
 redis_password_raw = ""
 redis_password_encoded = base64.b64encode(redis_password_raw.encode()).decode()
@@ -21,7 +20,3 @@ kafka_password_encoded = base64.b64encode(kafka_password_raw.encode()).decode()
 print("kafka_password_raw: ", kafka_password_raw)
 print("kafka_password_encoded: ", kafka_password_encoded)
 
-
-
-print(encoded_key) 
-print(mariadb_password_encoded)
