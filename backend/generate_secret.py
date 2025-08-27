99a79cf2b650dd203c5fe7a6e430812022f3eda8
rlfahs3025 <rlfahs3025@gmail.com>
2025-08-27 21:49:07 +0900

    Complete user login

--- backend/generate_secret.py
+++ backend/generate_secret.py
@@ -4,10 +4,10 @@ import base64
 secret_key = secrets.token_hex(32)
 encoded_key = base64.b64encode(secret_key.encode()).decode()
 
-mariadb_password_raw = "MyQ3n9NNA0%"
+mariadb_password_raw = ""
 mariadb_password_encoded = base64.b64encode(mariadb_password_raw.encode()).decode()
 
-print(mariadb_password_raw)
+print(mariadb_password_raw, mariadb_password_encoded)
 
 redis_password_raw = "GjA0PHx96N"
 redis_password_encoded = base64.b64encode(redis_password_raw.encode()).decode()
